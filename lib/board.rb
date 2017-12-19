bcf4490aa6fcc9329b1653c6c88e7b12dd029aa1
Brandon Brasson <rocksong622@gmail.com>
2017-12-19 18:23:54 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,2 @@
 # Define the variable board below.
-1 2 3
-4 5 6
-7 8 9
+Hello,World!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+
 RSpec.configure do |config|
   config.order = :default
 end
@@ -6,10 +7,11 @@ end
 def get_variable_from_file(file, variable)
   file_scope = binding
   file_scope.eval(File.read(file))
-  
+
   begin
     return file_scope.local_variable_get(variable)
   rescue NameError
     raise NameError, "local variable `#{variable}' not defined in #{file}."
-  end    
+  end
 end
+
